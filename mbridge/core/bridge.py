5bea0cf965edfc4d8c685c952950ecb64c781a82
Zuyue Fu <fuzuyue1995@gmail.com>
2025-09-16 15:26:54 -0700

    debugging

--- mbridge/core/bridge.py
+++ mbridge/core/bridge.py
@@ -153,6 +153,7 @@ class Bridge(ABC):
         for i, model in enumerate(models):
             # map local weight names to global weight names
             local_to_global_map = self._weight_name_mapping_mcore_local_to_global(model)
+            print(f"zyzyzy mb local_to_global_map: {local_to_global_map}")
             # map local weight names to huggingface weight names
             local_to_hf_map = {
                 k: self._weight_name_mapping_mcore_to_hf(v)
--- mbridge/models/qwen3next.py
+++ mbridge/models/qwen3next.py
@@ -1,11 +1,62 @@
 # Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
 
-from ..core import register_model
-from .qwen2moe import Qwen2MoEBridge
+from ..core import LLMBridge, register_model
 
 
 @register_model("qwen3_next")
-class Qwen3NextBridge(Qwen2MoEBridge):
+class Qwen3NextBridge(LLMBridge):
+    _DIRECT_MAPPING = {
+        "embedding.word_embeddings.weight": "model.embed_tokens.weight",
+        "decoder.final_layernorm.weight": "model.norm.weight",
+        "output_layer.weight": "lm_head.weight",
+    }
+    _ATTENTION_MAPPING = {
+        "self_attention.linear_proj.weight": [
+            "model.layers.{layer_number}.self_attn.o_proj.weight"
+        ],
+        "self_attention.linear_qkv.layer_norm_weight": [
+            "model.layers.{layer_number}.input_layernorm.weight"
+        ],
+        "self_attention.q_layernorm.weight": [
+            "model.layers.{layer_number}.self_attn.q_norm.weight"
+        ],
+        "self_attention.k_layernorm.weight": [
+            "model.layers.{layer_number}.self_attn.k_norm.weight"
+        ],
+        "self_attention.linear_qkv.weight": [
+            "model.layers.{layer_number}.self_attn.q_proj.weight",
+            "model.layers.{layer_number}.self_attn.k_proj.weight",
+            "model.layers.{layer_number}.self_attn.v_proj.weight",
+        ],
+        "self_attention.linear_qkv.bias": [
+            "model.layers.{layer_number}.self_attn.q_proj.bias",
+            "model.layers.{layer_number}.self_attn.k_proj.bias",
+            "model.layers.{layer_number}.self_attn.v_proj.bias",
+        ],
+    }
+    _MLP_MAPPING = {
+        "shared_experts.linear_fc1.weight": [
+            "model.layers.{layer_number}.mlp.shared_expert.gate_proj.weight",
+            "model.layers.{layer_number}.mlp.shared_expert.up_proj.weight",
+        ],
+        "pre_mlp_layernorm": [
+            "model.layers.{layer_number}.post_attention_layernorm.weight"
+        ],
+        "shared_experts.linear_fc2.weight": [
+            "model.layers.{layer_number}.mlp.shared_expert.down_proj.weight"
+        ],
+        "mlp.router.weight": ["model.layers.{layer_number}.mlp.gate.weight"],
+        "shared_experts.gate_weight": [
+            "model.layers.{layer_number}.mlp.shared_expert_gate.weight"
+        ],
+        "mlp.experts.linear_fc1": [
+            "model.layers.{layer_number}.mlp.experts.{expert_id}.gate_proj.weight",
+            "model.layers.{layer_number}.mlp.experts.{expert_id}.up_proj.weight",
+        ],
+        "mlp.experts.linear_fc2": [
+            "model.layers.{layer_number}.mlp.experts.{expert_id}.down_proj.weight"
+        ],
+    }
 
     def _build_config(self):
         return self._build_base_config(
@@ -28,3 +79,25 @@ class Qwen3NextBridge(Qwen2MoEBridge):
             moe_router_pre_softmax=False,
             qk_layernorm=True,
         )
+
+    def _weight_name_mapping_mlp(self, name: str) -> list[str]:
+        layer_number = name.split(".")[2]
+        convert_names = []
+        for keyword, mapping_names in self._MLP_MAPPING.items():
+            if keyword in name:
+                if "{expert_id}" in mapping_names[0]:
+                    expert_id = name.split("weight")[-1]
+                    convert_names.extend(
+                        [
+                            x.format(layer_number=layer_number, expert_id=expert_id)
+                            for x in mapping_names
+                        ]
+                    )
+                else:
+                    convert_names.extend(
+                        [x.format(layer_number=layer_number) for x in mapping_names]
+                    )
+                break
+        if len(convert_names) == 0:
+            raise NotImplementedError(f"Unsupported parameter name: {name}")
+        return convert_names
