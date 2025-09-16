bd3c15fdebd0764c228fa300a58fcd54c89baecd
Zuyue Fu <fuzuyue1995@gmail.com>
2025-09-15 18:03:58 -0700

    try qwewn3 next support

--- mbridge/models/__init__.py
+++ mbridge/models/__init__.py
@@ -17,6 +17,9 @@ Classes:
 # fmt: off
 
 from .deepseek_v3 import DeepseekV3Bridge
+from .gemma3 import Gemma3Bridge
+from .glm4_vl import Glm4VLBridgeDense, Glm4VLBridgeMoe
+from .glm4moe import GLM4MoEBridge
 from .llama import LLaMABridge
 from .mimo import MimoBridge
 from .mixtral import MixtralBridge
@@ -25,6 +28,4 @@ from .qwen2_5_vl import Qwen2_5VLBridge
 from .qwen2moe import Qwen2MoEBridge
 from .qwen3 import Qwen3Bridge
 from .qwen3moe import Qwen3MoEBridge
-from .glm4moe import GLM4MoEBridge
-from .glm4_vl import Glm4VLBridgeMoe, Glm4VLBridgeDense
-from .gemma3 import Gemma3Bridge
+from .qwen3next import Qwen3NextBridge
---
+++ mbridge/models/qwen3next.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+
+from ..core import register_model
+from .qwen2moe import Qwen2MoEBridge
+
+
+@register_model("qwen3_next")
+class Qwen3NextBridge(Qwen2MoEBridge):
+
+    def _build_config(self):
+        return self._build_base_config(
+            use_cpu_initialization=False,
+            # MoE specific
+            moe_ffn_hidden_size=self.hf_config.moe_intermediate_size,
+            moe_router_bias_update_rate=0.001,
+            moe_router_topk=self.hf_config.num_experts_per_tok,
+            num_moe_experts=self.hf_config.num_experts,
+            moe_aux_loss_coeff=self.hf_config.router_aux_loss_coef,
+            # moe_router_load_balancing_type="aux_loss",
+            moe_router_load_balancing_type="none",  # default None for RL
+            moe_grouped_gemm=True,
+            moe_router_score_function="softmax",
+            # Other optimizations
+            persist_layer_norm=True,
+            bias_activation_fusion=True,
+            bias_dropout_fusion=True,
+            # Qwen specific
+            moe_router_pre_softmax=False,
+            qk_layernorm=True,
+        )
