8e9152cf0c01300a98656b5903f7573254dae77b
Zuyue Fu <fuzuyue1995@gmail.com>
2025-09-16 19:29:35 -0700

    debugging

--- mbridge/core/bridge.py
+++ mbridge/core/bridge.py
@@ -153,7 +153,8 @@ class Bridge(ABC):
         for i, model in enumerate(models):
             # map local weight names to global weight names
             local_to_global_map = self._weight_name_mapping_mcore_local_to_global(model)
-            print(f"zyzyzy mb local_to_global_map: {local_to_global_map}")
+            for k, v in local_to_global_map.items():
+                print(f"[zyzyzy mb local_to_global_map] {k}: {v}")
             # map local weight names to huggingface weight names
             local_to_hf_map = {
                 k: self._weight_name_mapping_mcore_to_hf(v)
